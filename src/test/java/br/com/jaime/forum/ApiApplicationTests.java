07e8bb1d06dccc38d9783901e372b09fe328f51b
jaimebaldin <you@example.com>
2022-04-30 13:04:46 -0300

    criado o repositório inicial do projeto

---
+++ src/test/java/br/com/jaime/forum/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jaime.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
